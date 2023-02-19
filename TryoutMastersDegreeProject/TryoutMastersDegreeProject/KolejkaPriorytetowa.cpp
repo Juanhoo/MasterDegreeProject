7b4929e0159f3a96c56121fe767a177412ac8f88
Juanhoo <piotkal932@student.polsl.pl>
2023-02-19 13:22:37 +0100

    Complete link and single link update

---
+++ TryoutMastersDegreeProject/TryoutMastersDegreeProject/KolejkaPriorytetowa.cpp
@@ -0,0 +1,100 @@
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <unordered_set>
+// Priority queue includes track of the minimum distance between clusters rather than iterating trough all pairs -> O(n^3) to O(n^2logn)
+using DistanceMatrix = std::unordered_map<std::string, std::unordered_map<std::string, double>>;
+
+std::unordered_map<std::size_t, std::vector<std::string>> single_link(const DistanceMatrix& distances) {
+    std::unordered_map<std::size_t, std::vector<std::string>> clusters;
+    std::size_t cluster_id = 0;
+    // Initialize each point as its own cluster
+    for (const auto& point : distances) {
+        clusters[cluster_id++] = std::vector<std::string>{ point.first };
+    }
+    // Create a priority queue to track the minimum distance between clusters
+    using ClusterDistance = std::pair<std::size_t, std::size_t>;
+    auto cmp = [&](const ClusterDistance& c1, const ClusterDistance& c2) {
+        return distances.at(clusters[c1.first].front()).at(clusters[c1.second].front()) >
+            distances.at(clusters[c2.first].front()).at(clusters[c2.second].front());
+    };
+    std::priority_queue<ClusterDistance, std::vector<ClusterDistance>, decltype(cmp)> pq(cmp);
+    for (auto i_c = clusters.begin(); i_c != clusters.end(); ++i_c) {
+        const std::size_t i = i_c->first;
+        const auto& ci = i_c->second;
+        for (auto j_c = std::next(i_c); j_c != clusters.end(); ++j_c) {
+            const std::size_t j = j_c->first;
+            pq.emplace(i, j);
+        }
+    }
+    // Merge clusters until there is only one left
+    while (clusters.size() > 1) {
+        // Find the closest pair of clusters using the priority queue
+        std::size_t min_i = pq.top().first;
+        std::size_t min_j = pq.top().second;
+        pq.pop();
+        if (clusters.find(min_i) == clusters.end() || clusters.find(min_j) == clusters.end()) {
+            // One of the clusters has already been merged, skip this pair
+            continue;
+        }
+        // Merge the two closest clusters
+        clusters[min_i].insert(clusters[min_i].end(), clusters[min_j].begin(), clusters[min_j].end());
+        clusters.erase(min_j);
+        // Update the priority queue with the new distances to the merged cluster
+        for (const auto& c : clusters) {
+            if (c.first != min_i) {
+                pq.emplace(min_i, c.first);
+            }
+        }
+    }
+    return clusters;
+
+}
+
+    ////////////////////// Complete link 
+    using DistanceMatrix = std::unordered_map<std::string, std::unordered_map<std::string, double>>;
+
+    std::unordered_map<std::size_t, std::vector<std::string>> complete_link(const std::unordered_map<std::string, std::unordered_map<std::string, double>>&distances) {
+        std::unordered_map<std::size_t, std::vector<std::string>> clusters;
+        std::size_t cluster_id = 0;
+        // Initialize each point as its own cluster
+        for (const auto& point : distances) {
+            clusters[cluster_id++] = std::vector<std::string>{ point.first };
+        }
+        // Create a priority queue to track the minimum distance between clusters
+        using ClusterDistance = std::pair<std::size_t, std::size_t>;
+        auto cmp = [&](const ClusterDistance& c1, const ClusterDistance& c2) {
+            return distances.at(clusters[c1.first].front()).at(clusters[c1.second].front()) >
+                distances.at(clusters[c2.first].front()).at(clusters[c2.second].front());
+        };
+        std::priority_queue<ClusterDistance, std::vector<ClusterDistance>, decltype(cmp)> pq(cmp);
+        for (auto i_c = clusters.begin(); i_c != clusters.end(); ++i_c) {
+            const std::size_t i = i_c->first;
+            const auto& ci = i_c->second;
+            for (auto j_c = std::next(i_c); j_c != clusters.end(); ++j_c) {
+                const std::size_t j = j_c->first;
+                pq.emplace(i, j);
+            }
+        }
+        // Merge clusters until there is only one left
+        while (clusters.size() > 1) {
+            // Find the closest pair of clusters using the priority queue
+            std::size_t min_i = pq.top().first;
+            std::size_t min_j = pq.top().second;
+            pq.pop();
+            if (clusters.find(min_i) == clusters.end() || clusters.find(min_j) == clusters.end()) {
+                // One of the clusters has already been merged, skip this pair
+                continue;
+            }
+            // Merge the two closest clusters
+            clusters[min_i].insert(clusters[min_i].end(), clusters[min_j].begin(), clusters[min_j].end());
+            clusters.erase(min_j);
+            // Update the priority queue with the new distances to the merged cluster
+            for (const auto& c : clusters) {
+                if (c.first != min_i) {
+                    pq.emplace(min_i, c.first);
+                }
+            }
+        }
+        return clusters;
+    }
--- TryoutMastersDegreeProject/TryoutMastersDegreeProject/TryoutMastersDegreeProject.cpp
+++ TryoutMastersDegreeProject/TryoutMastersDegreeProject/TryoutMastersDegreeProject.cpp
@@ -1,153 +1,74 @@
-﻿// TryoutMastersDegreeProject.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <unordered_map>
+#include <vector>
 #include <string>
+#include <limits>
 
-typedef std::unordered_map<std::string, double> templateInside;
-typedef std::unordered_map<std::string, templateInside> templateMap;
-typedef std::unordered_map<std::string, std::string> closestPointsTemplate;
+using DistanceMatrix = std::unordered_map<std::string, std::unordered_map<std::string, double>>;
 
-
-int main()
-{
-    templateMap points;
-    templateInside dist;
-    std::string name = "test";
-    std::string zew = "main";
-    std::unordered_map<std::string, std::string> closestNeighboursMap;
-    //      A   B   C   D   E
-    // A    0   10  5   6   2
-    // B   10   0   3   4   1
-    // C    5   3   0   18  7
-    // D    6   4   18  0   9
-    // E    2   1   7   9   0 
-
-    templateInside LA = { {"A", INT_MAX}, { "B", 10 } , { "C", 5 }, { "D", 6 }, { "E", 2 } };
-    templateInside LB = { {"A", 10}, { "B", INT_MAX } , { "C", 3 }, { "D", 4 }, { "E", 1 } };
-    templateInside LC = { {"A", 5}, { "B", 3 } , { "C", INT_MAX }, { "D", 18 }, { "E", 7 } };
-    templateInside LD = { {"A", 6}, { "B", 4 } , { "C", 18 }, { "D", INT_MAX }, { "E", 9 } };
-    templateInside LE = { {"A", 2}, { "B", 1 } , { "C", 7 }, { "D", 9 }, { "E", INT_MAX } };
-
-    templateMap mainTable = { {"A", LA}, { "B", LB } , { "C", LC }, { "D", LD }, { "E", LE } };
-
-    auto test = mainTable;
-    //closestNeigbour[iter->second] << "closest od iter" <<
-
-    std::unordered_map<std::string, std::string> closestPoints = { {"A","E"},{"B","E"},{"C","B"},{"D","B"},{"E","B"} };
-
-    templateInside::const_iterator minimal = mainTable.begin()->second.begin(); // pkt [0][0]
-    templateMap::const_iterator it = test.begin();
-    templateInside::const_iterator iter = it->second.begin();
-    for (auto coppy = it; coppy != test.end(); ++coppy) {
-        for (auto inCoppy = iter; inCoppy != coppy->second.end(); ++inCoppy) {
-            //std::cout << iter->second << "\t";
-            auto candidate = closestPoints.find(coppy->first);
-            std::string closestNeigbour = candidate->second;
-            auto count = coppy->second.find(closestNeigbour);
-            auto data = coppy->second.find(inCoppy->first);
-            std::cout << "Data: " << data->second << std::endl;
-            if (minimal->second > inCoppy->second) {
-                std::cout << minimal->second << "<" << inCoppy->second << std::endl;
-                minimal = inCoppy;
-                std::cout << minimal->first << std::endl;  
-            }
-           
-        }
-        std::cout << "next stage" << std::endl;
-        
+std::unordered_map<std::size_t, std::vector<std::string>> complete_link(const DistanceMatrix& distances) {
+    std::unordered_map<std::size_t, std::vector<std::string>> clusters;
+    std::size_t cluster_id = 0;
+    // Initialize each point as its own cluster
+    for (const auto& point : distances) {
+        clusters[cluster_id++] = std::vector<std::string>{ point.first };
     }
-
-    auto considerBpoint = closestPoints.begin();
-    std::string pointB;
-    double distance = INT_MAX;
-    for (auto closestIterator = considerBpoint; closestIterator != closestPoints.end(); ++closestIterator) {
-        if (closestIterator->second == minimal->first) {
-            auto candidate = it->second.find(closestIterator->second);
-            if (minimal->second > candidate->second) {
-                std::cout << "Minimal " << minimal->second << " candidate" << candidate->second << std::endl;
+    // Merge clusters until there is only one left
+    while (clusters.size() > 1) {
+        double max_distance = std::numeric_limits<double>::min();
+        std::size_t max_i = 0, max_j = 0;
+        // Find the closest pair of clusters
+        for (const auto& i_c : clusters) {
+            const std::size_t i = i_c.first;
+            const auto& ci = i_c.second;
+            for (const auto& j_c : clusters) {
+                const std::size_t j = j_c.first;
+                const auto& cj = j_c.second;
+                if (i >= j) {
+                    continue;
+                }
+                double distance = std::numeric_limits<double>::min();
+                // Calculate the distance between each pair of points in the clusters
+                for (const auto& p1 : ci) {
+                    for (const auto& p2 : cj) {
+                        distance = std::max(distance, distances.at(p1).at(p2));
+                    }
+                }
+                // Update the maximum distance and indices if necessary
+                if (distance > max_distance) {
+                    max_distance = distance;
+                    max_i = i;
+                    max_j = j;
+                }
             }
         }
+        // Merge the two closest clusters
+        clusters[max_i].insert(clusters[max_i].end(), clusters[max_j].begin(), clusters[max_j].end());
+        clusters.erase(max_j);
+    }
+    return clusters;
+}
 
+
+int main() {
+    // Construct the distance matrix
+    DistanceMatrix distances = {
+         {"A", {{"A", 0.0}, {"B", 2.0}, {"C", 1.0}}},
+         {"B", {{"A", 2.0}, {"B", 0.0}, {"C", 4.0}}},
+         {"C", {{"A", 1.0}, {"B", 4.0}, {"C", 0.0}}},
+    };
+
+    // Compute the complete-link clustering
+    auto clusters = complete_link(distances);
+
+    // Print the resulting clusters
+    for (const auto& c : clusters) {
+        std::cout << "Cluster " << c.first << ": ";
+        for (const auto& p : c.second) {
+            std::cout << p << " ";
+        }
+        std::cout << std::endl;
     }
 
-    //    for (closestPointsTemplate::const_iterator it = closestPoints.begin(); it != closestPoints.end(); ++it) {
-    //        std::cout << "From " << it->first << "is closest to " << it->second << std::endl;
-    //    }
-    //
-    //
-    //    for (templateMap::const_iterator it = test.begin(); it != test.end(); ++it) {
-    //        for (templateInside::const_iterator iter = it->second.begin(); iter != it->second.end(); ++iter) {
-    //            std::cout << iter->second << "\t";
-    //            auto candidate = closestPoints.find(it->first);
-    //            //it->second.find(candidate);
-    //            //std::cout << candidate->first << '\t' << candidate->second << '\n';
-    //            //std::cout << it->second[candidate];
-    //            std::string closestNeigbour = candidate->second;
-    //            auto count = it->second.find(closestNeigbour);
-    //            std::cout << "" << iter->first << "Closest " << count->first << std::endl;
-    //
-    //        }
-    //    }
-    //    //for (templateMap::const_iterator it = mainTable.begin(); it != mainTable.end(); ++it) {
-    //    //    std::cout << it->first << std::endl;
-    //    //    auto candidate = closestPoints.find(it->first);
-    //    //    //it->second.find(candidate);
-    //    //    std::cout << candidate->first << '\t' << candidate->second << '\n';
-    //    //    //std::cout << it->second[candidate];
-    //    //    std::string closestNeigbour = candidate->second;
-    //    //    auto count = it->second.find(closestNeigbour);
-    //    //    //for (auto&iter = it->second; iter != it->second.end(); )
-    //    //    for (auto& iter : it->second) {
-    //    //        if (iter.second > count->second)
-    //    //            std::cout << "Znaleziono" << std::endl;
-    //    //        std::cout << it->first << " to " << iter.first << " takes " << iter.second << '\t';
-    //    //        
-    //    //    }  
-    //    //    std::cout <<  std::endl;
-    //
-    //    //}
-    //
-    //    //for (int i = 0; i < 10; i++) {
-    //    //    //closestNeighboursMap.insert(templateMap::value_type(name, zew));
-    //    //    for (int j = 0; j < 10; j++) {
-    //    //        dist.insert(templateInside::value_type(name, rand() % 25));
-    //    //        name += 't';
-    //    //    }
-    //    //    points.insert(templateMap::value_type(zew, dist));
-    //    //    name = "test";
-    //    //    zew += 'w';
-    //    //}
-    //
-    //    for (templateMap::const_iterator it = points.begin(); it != points.end(); ++it) {
-    //        for (templateInside::const_iterator iter = it->second.begin(); iter != it->second.end(); ++iter) {
-    //            //std::cout << it->first << "\t" << iter->first << "\t" << iter->second << std::endl;
-    //        }
-    //
-    //    }
-    //
-    //    templateMap::const_iterator it = points.begin();
-    //
-    //
-    //    while (points.size() > 1) {
-    //
-    //        for (templateMap::const_iterator it = points.begin(); it != points.end(); ++it) {
-    //            auto minimalCandidate = closestNeighboursMap[it->first];
-    //            int minimum = it->second.find(minimalCandidate)->second;
-    //            //wyszukaj minimum dla aktualnego kandydata
-    //            for (templateInside::const_iterator itin = it->second.begin(); itin != it->second.end(); ++itin) {
-    //                if (minimum > itin->second) {
-    //                    minimalCandidate = itin->first;
-    //                    minimum = itin->second;
-    //                }
-    //            }
-    //           // closestNeighboursMap[it->second] = minimalCandidate;
-    //
-    //        }
-    //    }
-    //
-    //
-    //}
-    //
+    return 0;
 }
